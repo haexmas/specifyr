9314170f90bb62530aabc2df67b91f143b2dfcac
Martin Drechsel <mdrechsel@itemis.com>
2026-09-06 13:44:47 +0200

    Re-export core schemas from src/core/index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,36 @@
-export {};
+export {
+  AttributeSchema,
+  ClassSchema,
+  EdgeSchema,
+  MethodSchema,
+  MODEL_SOURCES,
+  ModelMetaSchema,
+  ModelSchema,
+  NODE_ID_PATTERN,
+  NodeSchema,
+} from "./schemas.ts";
+export type {
+  Attribute,
+  Class,
+  Edge,
+  Method,
+  Model,
+  ModelMeta,
+  ModelNode,
+  ModelSource,
+  Node,
+} from "./schemas.ts";
+
+export {
+  AttributeDefinitionSchema,
+  CustomTypeSchema,
+  PackNameSchema,
+  SHIPPED_PACKS,
+  VocabularyConfigSchema,
+} from "./vocabulary.ts";
+export type {
+  AttributeDefinition,
+  CustomType,
+  PackName,
+  VocabularyConfig,
+} from "./vocabulary.ts";

cdd07d27f182d5f4b3c80894acdba98031f0b82a
Martin Drechsel <mdrechsel@itemis.com>
2026-09-06 15:26:31 +0200

    Re-export storage API from src/storage/index.ts

---
+++ src/storage/index.ts
@@ -0,0 +1,6 @@
+export { bucketForNode, SUPPORTED_NODE_TYPES } from "./bucket.ts";
+export type { SollBucket, SollLayout, SupportedNodeType } from "./bucket.ts";
+
+export { resolveInsideRoot, sollRoot } from "./paths.ts";
+
+export { loadSoll, saveSoll } from "./soll.ts";

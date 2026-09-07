170b7caed7a8228909e4f757d9140eed75198b61
Martin Drechsel <mdrechsel@itemis.com>
2026-09-07 12:53:06 +0200

    Re-export TypeScript IST extractor API

---
+++ src/extractors/typescript/index.ts
@@ -0,0 +1,5 @@
+export { extractIst } from "./extract.js";
+export { extractSource } from "./extract-source.js";
+export { istNodeId } from "./node-id.js";
+export { parseTypeScript } from "./parser.js";
+export { SKIP_DIRS, walkTsFiles } from "./walk.js";
